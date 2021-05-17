31d982c20948e51fc260f6c9fee3e6eafb699305
mirae <miraecod@gmail.com>
2021-05-17 23:04:51 +0900

    [Create] Image components

---
+++ src/components/image/ItemImg.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ItemImage = styled.img`
+  width: 320px;
+  height: 210px;
+  border-radius: 5px;
+`
+
+export default ItemImage
---
+++ src/components/image/ThumbnailNewsImg.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const ThumbnailNewsImage = styled.img`
+  width: 130px;
+  height: 130px;
+  border-radius: 5px;
+  cursor: pointer;
+`
+
+export default ThumbnailNewsImage
