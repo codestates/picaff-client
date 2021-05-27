6176343ea4b5095eee58c2ac3658443198ddf6fa
jeonghun-project <dsjoh111@gmail.com>
2021-05-27 11:24:02 +0900

    [Refactor] Cleint Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-import Sample from './Sample'
 import Navbar from 'components/nav/Navbar'
+import Landing from 'components/main/Landing'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import TestImpls from 'containers/testpage/TestImpls'
+import Sample from 'Sample'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Navbar />
-      <Sample />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Landing />
+          </Route>
+          <Route path='/test'>
+            <TestImpls />
+          </Route>
+          <Route path='/sample'>
+            <Sample />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   )
 }
--- src/Sample.tsx
+++ src/Sample.tsx
@@ -3,7 +3,7 @@ import CoffeeMap from 'components/coffee-map/CoffeeMap'
 export default function Sample() {
   return (
     <div style={{ width: '100%', height: '100%' }}>
-      <CoffeeMap type='KE' />
+      <CoffeeMap type='All' />
     </div>
   )
 }
