7c826dacca513c42b358752b10747ce04125b600
mirae <miraecod@gmail.com>
2021-06-01 21:02:23 +0900

    [Style] Result container layout

--- src/containers/result/ProductResult.tsx
+++ src/containers/result/ProductResult.tsx
@@ -47,69 +47,99 @@ export default function ProductResult({ data }: Props) {
     getAllItems()
   }, [])
 
-  // 태그 클릭시 해당 태그를 갖고 있는 아이템 리스트를 받아옴
-  // useEffect(() => {
-  //   const getSelectedTags = async () => {
-  //     const res = await axios.get(
-  //       `https://localhost:4000/item/tag?itemType=product&tagId=${selectedTag}`,
-  //       {}
-  //     )
-  //     if (res.status === 200) {
-  //       // 태그버튼 클릭시 요청 보냄 - response로 오는 id와 이미지버튼 key값이랑 같은지 체크
-  //       // => 같을 경우 className 추가하는 형식으로 스타일 효과주기
-  //       // 태그클릭 state 생성 필요
-  //     }
-  //   }
-  //   getSelectedTags()
-  // }, [])
-
   return (
-    <>
-      <ProductResultContainer className='section_product'>
-        <section className='section_result'>
-          <div className='description'>
-            <div className='box_image'>
-              <img src={data.imageUrl}></img>
+    <ProductResultContainer className='section_product'>
+      <section className='section_result'>
+        <div className='box_image'>
+          <img src={data.imageUrl}></img>
+        </div>
+        <div className='parent_desc'>
+          <div className='box_desc'>
+            <div className='name'>{data.itemName}</div>
+            <div className='text'>
+              {data.itemDetail}는 빠르고 간단하며 일반 추출법과 다르게 머신에서 사용 가능한 캡슐을
+              넣은 후 버튼만 눌러주면 에스프레소가 추출됩니다. 항상 일정한 품질의 에스프레소 추출이
+              가능하며, 캡슐은 원두와 다르게 상온에 보관이 가능한 장점이 있습니다. 상대적으로
+              에스프레소 머신이나, 핸드드립을 사용 할 때 사용되는 그라인더나 여러가지 용품의 가격을
+              비교하면 저렴한 편에 속합니다. 그러나 개별 캡슐의 가격이 조금 비싼편으로, 커피를 자주
+              마실수록 한 잔에 대한 금액이 부담 될 수 있습니다.
             </div>
-            <div className='box_tag'>
+            <div className='tag'>
               {data.tag.map((singleTag: Tags) => (
-                <Tag
-                  style='ClearTag'
-                  key={singleTag.id}
-                  value={singleTag.tagName}
-                  // onClick={() => setSelectedTag(singleTag.id)}
-                />
+                <Tag style='ClearTag' key={singleTag.id} value={singleTag.tagName} />
               ))}
             </div>
-            <div className='box_text'>{data.itemDetail}</div>
           </div>
-          {radarInfo && <ProductRadarChart radarInfo={radarInfo} />}
-        </section>
-        <section className='section_image'>
-          {allItems.map((singleItem: itemResult) => (
-            <div>
-              <Image
-                style='ButtonImage'
-                type='button'
-                key={singleItem.id}
-                src={singleItem.imageUrl}
-                onClick={() => {
-                  setSelectedItem(singleItem)
-                  setIsItemClicked(!isItemClicked)
-                }}
-              />
-            </div>
-          ))}
-          {isItemClicked ? (
-            <ProductItem
-              selectedItem={selectedItem}
-              handlechecked={() => setIsItemClicked(!selectedItem)}
+        </div>
+        <div className='box_radar'>{radarInfo && <ProductRadarChart radarInfo={radarInfo} />}</div>
+      </section>
+      <section className='section_image'>
+        <div className='title'>더욱 다양한 용품들을 만나보세요.</div>
+        <div className='image_box'>
+          <div className='image' id='first'>
+            <Image
+              style='ButtonImage'
+              type='button'
+              key={allItems[0].id}
+              src={allItems[0].imageUrl}
+              onClick={() => {
+                setSelectedItem(allItems[0])
+                setIsItemClicked(!isItemClicked)
+              }}
             />
-          ) : (
-            ''
-          )}
-        </section>
-      </ProductResultContainer>
-    </>
+            <div className='singleName'>{allItems[0].itemName}</div>
+          </div>
+          <div className='image' id='second'>
+            <Image
+              style='ButtonImage'
+              type='button'
+              key={allItems[1].id}
+              src={allItems[1].imageUrl}
+              onClick={() => {
+                setSelectedItem(allItems[1])
+                setIsItemClicked(!isItemClicked)
+              }}
+            />
+            <div className='singleName'>{allItems[1].itemName}</div>
+          </div>
+        </div>
+        <div className='image_box'>
+          <div className='image' id='third'>
+            <Image
+              style='ButtonImage'
+              type='button'
+              key={allItems[2].id}
+              src={allItems[2].imageUrl}
+              onClick={() => {
+                setSelectedItem(allItems[2])
+                setIsItemClicked(!isItemClicked)
+              }}
+            />
+            <div className='singleName'>{allItems[2].itemName}</div>
+          </div>
+          <div className='image' id='fourth'>
+            <Image
+              style='ButtonImage'
+              type='button'
+              key={allItems[3].id}
+              src={allItems[3].imageUrl}
+              onClick={() => {
+                setSelectedItem(allItems[3])
+                setIsItemClicked(!isItemClicked)
+              }}
+            />
+            <div className='singleName'>{allItems[3].itemName}</div>
+          </div>
+        </div>
+      </section>
+      {isItemClicked ? (
+        <ProductItem
+          selectedItem={selectedItem}
+          handlechecked={() => setIsItemClicked(!selectedItem)}
+        />
+      ) : (
+        ''
+      )}
+    </ProductResultContainer>
   )
 }
