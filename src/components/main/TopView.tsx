4ffbfed57ff4544398be22b4622a722ff5be5378
mirae <miraecod@gmail.com>
2021-06-04 16:40:08 +0900

    [Fix] Main page service description

--- src/components/main/TopView.tsx
+++ src/components/main/TopView.tsx
@@ -13,7 +13,7 @@ export default function TopView() {
         <div className='subdec'>
           <div className='style'>
             <hr className='bar' />
-            <span>COFFEE RECOMMEND</span>
+            <span>PICK YOUR CAFE & COFFEE</span>
           </div>
         </div>
         <div className='Container dec'>
@@ -26,12 +26,14 @@ export default function TopView() {
         </div>
         <div className='Container p'>
           <p>
-            처음 시작하는 홈 카페 <br />
-            picaff와 함께 시작하세요
+            처음 시작하는 홈카페 <br />
+            뭐부터 해야 할지 고민되시나요? <br />
+            간단한 테스트를 통해 나의 취향을 알아보고 <br />
+            PICAFF와 함께 나만의 홈카페를 시작해보세요
           </p>
         </div>
         <div className='Container button' onClick={handleClick}>
-          <a className='btn'>커피취향 알아보기</a>
+          <a className='btn'>나의 홈카페 취향 알아보기</a>
         </div>
       </section>
       <div className='VideoContainer'>
